93aa6c1edcf66a1a85e1336919431ac53bd49aaf
Stuart Meacham <smeacham@ebi.ac.uk>
2018-02-13 14:04:06 +0000

    removed legacy/orphaned code

--- js/main.js
+++ js/main.js
@@ -18,17 +18,10 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     .when("/intro", {templateUrl: "partials/intro.html", controller: "PageCtrl"})
     .when("/practise", {templateUrl: "partials/practise.html", controller: "PageCtrl"})
     .when("/deepdive", {templateUrl: "partials/deepdive.html", controller: "PageCtrl"})
     // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
